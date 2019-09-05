567a66d30fb86ad144e3deec256e27d05a611c2d
Ben Morrison <ben@gbmor.dev>
2019-09-04 22:04:18 -0400

    error helper test

--- src/error.rs
+++ src/error.rs
@@ -10,3 +10,22 @@ where
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn shouldnt_panic() {
+        let ok: Result<&str, &str> = Ok("okay");
+        let rhs = helper(ok);
+        assert_eq!("okay", rhs);
+    }
+
+    #[test]
+    #[should_panic]
+    fn should_panic() {
+        let err: Result<&str, &str> = Err("oops");
+        helper(err);
+    }
+}
